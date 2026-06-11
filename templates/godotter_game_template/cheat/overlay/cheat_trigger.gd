5577b2309eefe4a0d4ddd5fa7cb0d723bbccc5b3
JodgeMin <1460881525@qq.com>
2026-06-11 15:37:23 +0800

    fix(cheat): remove parent_pos offset, CanvasLayer has no global_position

--- templates/godotter_game_template/cheat/overlay/cheat_trigger.gd
+++ templates/godotter_game_template/cheat/overlay/cheat_trigger.gd
@@ -29,9 +29,7 @@ func _process(_delta: float) -> void:
 	if not _is_dragging:
 		_is_dragging = gmp.distance_to(_drag_start_pos) > _tap_threshold
 	if _is_dragging:
-		var parent_layer: CanvasLayer = get_parent()
-		var parent_pos: Vector2 = parent_layer.global_position if parent_layer else Vector2.ZERO
-		position = gmp - _drag_offset - parent_pos
+		position = gmp - _drag_offset
 
 
 func _gui_input(event: InputEvent) -> void:
@@ -62,6 +60,4 @@ func _gui_input(event: InputEvent) -> void:
 		if not _is_dragging:
 			_is_dragging = (event.position - _drag_offset).length() > _tap_threshold
 		if _is_dragging:
-			var parent_layer: CanvasLayer = get_parent()
-			var parent_pos: Vector2 = parent_layer.global_position if parent_layer else Vector2.ZERO
-			position = event.position - _drag_offset - parent_pos + _drag_start_pos
+			position = event.position - _drag_offset + _drag_start_pos
